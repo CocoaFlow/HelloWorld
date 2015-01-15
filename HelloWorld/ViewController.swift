510d72fdbf7e8dc0e783dc08f5054a229495928a
Paul Young <paulyoungonline@gmail.com>
2015-01-14 21:04:26 -0800

    Set up the network.

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import Engine
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let network = Network()
+        
+        let actOnControlEvents = ActOnControlEvents(network)
+        let presentViewController = PresentViewController(network)
+        
+        network.addEdge(actOnControlEvents.actionPort, presentViewController.presentPort)
+        
+        
+        // Mark: - Button
+        
+        let button = UIButton.buttonWithType(.Custom) as UIButton
+        button.frame = CGRectMake(50, 50, 100, 50)
+        button.backgroundColor = UIColor.grayColor()
+        button.setTitle("Tap Me", forState: .Normal)
+        self.view.addSubview(button)
+
+        
+        // Mark: - Alert Controller
+        
+        let alertController = UIAlertController(title: "Hello", message: "World", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(okAction)
+        
+        
+        // Mark: - IIPs
+        
+        actOnControlEvents.controlPort.receive(button)
+        actOnControlEvents.eventsPort.receive(.TouchUpInside)
+        
+        presentViewController.presentingViewControllerPort.receive(self)
+        presentViewController.viewControllerToPresentPort.receive(alertController)
+        presentViewController.animatedPort.receive(true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
